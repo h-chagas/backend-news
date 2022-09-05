e386e9285204017662841943827509fc68a74067
h-chagas <hugochagasuk@gmail.com>
2022-09-05 19:55:52 +0100

    remove whitespaces + add app.use to handle custom errors

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const { getTopics } = require('./controllers/news.controllers');
+
 const app = express();
-
-
 
 app.get('/api/topics', getTopics);
 
-
-
-
 app.all('/*', (req, res) => {
     res.status(404).send({msg: 'route not found'})
+})
+
+//handle custom errors
+app.use(() => {
+    if (err.status && err.msg) {
+        res.status(err.status).send( {msg: err.msg} );
+    } else {
+        next(err);
+    }
 })
 
 app.use((err, req, res, next) => {
