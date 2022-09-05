d90c9dc53642a5d2a9bd217f9ec935022249cdc0
h-chagas <hugochagasuk@gmail.com>
2022-09-05 20:03:41 +0100

    add 404 error handle + add few tests to check 200 status of the array response

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,13 +8,30 @@ const testData = require('../db/data/test-data/index');
 beforeEach( () => seed(testData));
 afterAll( ()=> db.end());
 
-describe('GET /api/topics', () => { 
+describe('GET /api/topics', () => {
+    test('404: responds with a message not found if a bad path is used', () => { 
+        return request(app)
+        .get('/api/oops')
+        .expect(404)
+        .then( (response) => {
+            expect(response.body).toEqual({msg: 'route not found'})
+        })
+     })
     test('200: responds with an array of topics', () => {
         return request(app)
         .get('/api/topics')
         .expect(200)
         .then( (response) => {
-            expect(typeof response.body).toBe('object')
+            expect(typeof response.body).toBe('object');
+            expect(response.body.length > 0).toBe(true);
+            expect(Array.isArray(response.body)).toBe(true);
+            response.body.forEach( (topic) => {
+                expect(topic).toMatchObject({
+                    slug: expect.any(String),
+                    description: expect.any(String),
+                })
+            })
+
         })
     })
  })
