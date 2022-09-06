a873bd61869311ff7c06b87d9ce6f840ae054374
h-chagas <hugochagasuk@gmail.com>
2022-09-06 10:47:24 +0100

    remove handle custom error and internal server error

--- app.js
+++ app.js
@@ -9,17 +9,5 @@ app.all('/*', (req, res) => {
     res.status(404).send({msg: 'route not found'})
 })
 
-//handle custom errors
-app.use(() => {
-    if (err.status && err.msg) {
-        res.status(err.status).send( {msg: err.msg} );
-    } else {
-        next(err);
-    }
-})
-
-app.use((err, req, res, next) => {
-    res.status(500).send( {msg: 'Internal server error!'} )
-})
 
 module.exports = app;
