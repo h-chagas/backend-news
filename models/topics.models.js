3b8f389dbb5fc3f8e2c9f569339ac2ef8ae8087f
h-chagas <hugochagasuk@gmail.com>
2022-09-06 14:22:45 +0100

    small modification in the result object - not destructure chosen

--- models/topics.models.js
+++ models/topics.models.js
@@ -3,7 +3,7 @@ const db = require('../db/connection')
 exports.showTopics = () => {
     return db
     .query('SELECT * FROM topics;')
-    .then( (result) => {
-        return result.rows
+    .then( ({rows}) => {
+        return rows
     })
 }
