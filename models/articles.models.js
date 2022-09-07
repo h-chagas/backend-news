e5319f05e3f783533b0f9e6a9bc6914fab468ab7
h-chagas <hugochagasuk@gmail.com>
2022-09-07 14:55:15 +0100

    refactor sql query statement in order to have comment_out column with articles votes in it

--- models/articles.models.js
+++ models/articles.models.js
@@ -2,7 +2,12 @@ const db = require("../db/connection");
 
 exports.showArticlesById = (article_id) => {
    return db
-   .query(`SELECT * FROM articles WHERE article_id=$1;`, [article_id])
+   .query(`SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.body, articles.created_at, articles.votes,
+   COUNT (comments.article_id)::INT AS comment_count
+   FROM articles
+   LEFT JOIN comments ON articles.article_id = comments.article_id 
+   WHERE articles.article_id=$1
+   GROUP BY articles.article_id;`, [article_id])
    .then((result) => {
     if(result.rowCount === 0) {
         return Promise.reject({ status: 404, msg: 'Article not found! Please try again' })
